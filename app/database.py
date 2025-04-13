3903554eec21a45ef240beb7e7aca7aa0ee75337
abraham <abrahamlawson67@gmail.com>
2025-04-13 19:46:57 +0200

    fix: change adress ip for database

--- app/database.py
+++ app/database.py
@@ -2,9 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://fastapi_user:fastapi_pass@mysql:3306/fastapi_db"
-
-# Init the connection with dataBase
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://fastapi_user:fastapi_pass@127.0.0.1:3306/fastapi_db"# Init the connection with dataBase
 try:
     engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
     print("Database connection successful!")
